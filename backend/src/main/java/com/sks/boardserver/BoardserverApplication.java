6d13bee883f1f2e981dc4ef4f0b7004028f75a7e
Junseok0326 <wnstjr7395@naver.com>
2022-07-14 23:57:43 +0900

    spring boot 환경 추가

---
+++ backend/src/main/java/com/sks/boardserver/BoardserverApplication.java
@@ -0,0 +1,13 @@
+package com.sks.boardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardserverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sks/boardserver/BoardserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sks.boardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
